77dde376e0a77733c768b55230e650490e9750bf
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-07-10 22:07:56 +0200

    feat: CustomInput component and text styles

---
+++ app/src/main/java/com/example/theaudiodbapp/components/CustomInput.kt
@@ -0,0 +1,37 @@
+package com.example.theaudiodbapp.components
+
+import android.content.Context
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.view.children
+import com.example.theaudiodbapp.R
+
+class CustomInput(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+    private val et: EditText
+    private val btnCancel: ImageView
+    var onTextChange: TextWatcher? = null
+        set(value) {
+            et.removeTextChangedListener(field)
+            et.addTextChangedListener(value)
+            field = value
+        }
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.custom_input, this)
+
+        et = findViewById(R.id.etCustomInput)
+        btnCancel = findViewById(R.id.btnCancelCustomInput)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        btnCancel.setOnClickListener { et.text.clear() }
+    }
+
+}
