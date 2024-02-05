548bfe8a2bfb7e00ecd74a3f2a8181cfbcdaa21f
DIALLO <ousmane.diallo1.sio@gmail.com>
2024-02-05 16:12:19 +0100

    tests: added test lanes

--- app/src/androidTest/java/com/example/theaudiodbapp/ui/home/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/example/theaudiodbapp/ui/home/HomeFragmentTest.kt
@@ -1,10 +1,12 @@
 package com.example.theaudiodbapp.ui.home
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.theaudiodbapp.MainActivity
 import com.example.theaudiodbapp.R
 import org.junit.Assert.*
@@ -14,7 +16,7 @@ import org.junit.Test
 class HomeFragmentTest {
 
     @get: Rule
-    val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun test_isHomeFragmentDisplayed() {
