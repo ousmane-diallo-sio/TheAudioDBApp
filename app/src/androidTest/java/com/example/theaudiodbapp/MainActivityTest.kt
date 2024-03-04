e7d5c0c4a34359ab8e61c2a318cf69f19c7055f4
Aaldn <aaldn.dev@gmail.com>
2024-03-04 21:50:17 +0100

    ci: implement screengrab and version bumper

--- app/src/androidTest/java/com/example/theaudiodbapp/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/theaudiodbapp/MainActivityTest.kt
@@ -2,11 +2,13 @@ package com.example.theaudiodbapp
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.junit.Assert.*
 import org.junit.Test
+import tools.fastlane.screengrab.Screengrab
 
 class MainActivityTest {
     @Test
@@ -14,5 +16,13 @@ class MainActivityTest {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
         onView(withId(R.id.cl_root_main)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testTakeScreenshot() {
+        Screengrab.screenshot("main_activity_screenshot")
+        //Screengrab.screenshot("before_button_click")
+        //onView(withId(android.R.id.home)).perform(ViewActions.click())
+        //Screengrab.screenshot("after_button_click")
     }
 }
